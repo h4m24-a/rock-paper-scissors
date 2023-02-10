3a6c43234f2698f716fa1986d356d0ab915e0cac
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-02-10 21:38:30 +0000

    updated playRound function with all possible outcomes

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const playRound =(playerSelection, computerSelection) => {
         return 'You tied! You both picked scissors' 
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return 'You tied! You both picked paper' 
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lost! Rock crushes Scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You won! Scissors cuts Paper'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lost! Paper covers Rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You won! Rock crushes Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lost! Scissors cuts Paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You won! Paper covers Rock'
     }
 }
