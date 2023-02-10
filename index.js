6d99f783d3086d391d45b4767249d547ab16f3c6
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-02-10 22:58:24 +0000

    added 5 rounds that reports win, loss or tie at the end

--- index.js
+++ index.js
@@ -16,20 +16,41 @@ const playRound =(playerSelection, computerSelection) => {
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return 'You tied! You both picked paper' 
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        //
+    compScore++
         return 'You lost! Rock crushes Scissors'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore++
         return 'You won! Scissors cuts Paper'
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    compScore++
         return 'You lost! Paper covers Rock'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You won! Rock crushes Scissors'
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    compScore++
         return 'You lost! Scissors cuts Paper'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore++
         return 'You won! Paper covers Rock'
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose what to throw : Rock, Paper, Scissors').toLowerCase()
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+     }
+
+     if (playerScore > compScore) {
+        return 'You beat the computer! You are a genius'
+     } else if (playerScore < compScore) {
+        return 'You got beat by the computer! You need to practice more'
+     } else {
+       return 'You tied with the computer'
+     }
+}
+
+console.log(game())
