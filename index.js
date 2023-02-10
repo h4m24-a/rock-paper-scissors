0f6cc5cc2f3b8d71553a818ca4a9727c1f5d7e1a
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-02-10 18:35:30 +0000

    testing to see if the function returns rock paper scissors

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const computerPlay = () => {
     const choices = ['rock', 'paper', 'scissors']
+    const randomNum = Math.floor(Math.random() * 3)
+    console.log(choices[randomNum])
 }
+
+computerPlay()
