75393c6bfb86001865d6a0d48adc2f8f1d6e254f
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-02-10 20:05:25 +0000

    updating round function with  ties

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ const computerPlay = () => {
 const playRound =(playerSelection, computerSelection) => {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         return 'You tied! You both picked rock'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You tied! You both picked scissors' 
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked paper' 
     }
 }
